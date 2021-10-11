2004afc0bbb07645e4fb8088461a96517870c4d2
nehiro <hiroaki.nerio@gmail.com>
2021-10-12 01:43:28 +0900

    firstup

---
+++ src/Counter.tsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useRef, useState } from 'react';
+
+const Counter: React.FC<{}> = () => {
+  const initialValue: any = 0;
+  const [value, setValue] = useState<number>(initialValue);
+
+  const increment = () => {
+    setValue((prevState) => prevState + 1);
+  };
+  const decrement = () => {
+    setValue((prevState) => prevState - 1);
+  };
+
+  const renderTimes = useRef<number>(0);
+  useEffect(() => {
+    renderTimes.current = renderTimes.current + 1;
+  });
+
+  const ref = useRef<HTMLInputElement>(null!);
+
+  const foucusInput = () => {
+    ref.current.focus();
+
+    // ref.current?.focus();
+
+    // const current = ref.current;
+    // if (current != null) {
+    //   current.focus();
+    // }
+  };
+  return (
+    <div>
+      <div>value: {value}</div>
+      <button onClick={increment}>+1</button>
+      <button onClick={decrement}>-1</button>
+      <div>This component was re-rendered {renderTimes.current} times!</div>
+      <input ref={ref} type="text" />
+      <button onClick={foucusInput}>Click me!</button>
+    </div>
+  );
+};
+
+export default Counter;
